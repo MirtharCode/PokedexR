f32db5bf20e439842731917a85c08f93464f3cd3
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-04-10 14:54:50 +0200

    Subido Readme

--- JS/listPokemon.js
+++ JS/listPokemon.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    // array donde se irán guardando los Pokémon de forma ordenada por ID
+    let sortedPokemon = [];
+    
     // Primera llamada a la API para conseguir el listado
     fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
         .then(function(response){
@@ -11,12 +14,60 @@ $(document).ready(function(){
             
             //Lanzamos una nueva petición a la API para cada Pokémon del listado que hemos recibido.
             pokemonList.forEach(function(pokemon){
-                fetchPokemonDetails(pokemon);
+                fetchAndSortPokemon(pokemon);
             });
         })
         .catch(function(err){
             console.log(err);
         });
+
+        // Función que pide a la API los datos de un Pokémon y los va ordenando en un array
+        function fetchAndSortPokemon(pokemon){
+            let urlPokemon = pokemon.url;
+
+            // Con esta llamada pedimos los detalles de cada Pokémon
+            fetch(urlPokemon)
+                .then(function(response){
+                    return response.json();
+                })
+                .then(function(pokemonDetails){
+                    console.log(pokemonDetails);
+                    
+                    // Insertamos el Pokémon con sus datos en el array sortedPokemon
+                    sortedPokemon.push(pokemonDetails);
+                    sortedPokemon.sort(function(a,b){
+                        return a.id-b.id;   // Ordenar por ID en orden ascendente.
+                    })
+
+                    // Llamamos a la función para renderizar el HTML de cada card
+                    renderPokemonCard();
+                })
+                .catch(function(err){
+                    console.log(err);
+                });
+        }
+
+        // Función para renderizar los datos de cada Pokémon en un car de BS
+        // Esta función recorrerá el array sortedPokemon
+        function renderPokemonCard(){
+            $("#pokeCont").empty();
+
+            sortedPokemon.forEach(function(pokemonDetails){
+                let pokeHTML =`
+                    <div class="card" style="width: 18rem;">
+                        <img src="${pokemonDetails.sprites.front_default}" class="card-img-top"     alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${pokemonDetails.name}</h5>
+                            <p class="card-text">${pokemonDetails.id}</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+            `;
+
+                $("#pokeCont").append(pokeHTML);
+            });
+        }
+
 
         // Función que se encarga de pedir los datos de cada Pokémon
         function fetchPokemonDetails(pokemon){
--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Si pongo data-loquesea es para crearme una caja contenedora a la que luego llama
 PokeApi
 
 model-viewer => integrar 3D en Web
+
+- Añadir un spinner (loading) de BS al #pokeCont.
+
+- Convertir la primera letra del nombre de cada Pokémon en mayúsculas. Esto lo haremos en la función renderPokemonCard(). PISTA -> buscaar función toUpperCase() de JS.
+
+- Crear botones de páginas que lancen la función listPokemon.js. PISTA -> mirar en la documentación de la PokeAPI las instrucciones de paginación.
+
+- Volver a activar y que funcione el isotopeJS.
